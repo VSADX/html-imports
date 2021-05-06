980f50d8ec845b6d2c78c4a34c448605ab149d77
VSADX <81491645+VSADX@users.noreply.github.com>
2021-05-06 14:16:42 -0500

    Update html-imports.js

--- html-imports.js
+++ html-imports.js
@@ -1,10 +1,11 @@
-export function html_imports() {
-const links = document.querySelectorAll("a[rel='text/html'], link[rel='text/html']")
-const actions = []
-links.forEach(link => 
-  actions.push(fetch(link.href)
-    .then(response => response.text())
-    .then(textHTML => link.outerHTML = textHTML)))
-Promise.all(actions)
+export function html_imports(doc = document, optional_function = () => {}) {
+  const links = document.querySelectorAll("a[rel='text/html'], link[rel='text/html']")
+  const actions = []
+  links.forEach(link => 
+    actions.push(fetch(link.href)
+      .then(response => response.text())
+      .then(textHTML => link.outerHTML = textHTML)))
+      .then(() => optional_function(link))
+  Promise.all(actions)
 }
 html_imports()
